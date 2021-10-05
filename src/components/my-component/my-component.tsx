ccf0bc31314bd68efdd235d300dd3285f75842ee
Mario Hamann <mario.hamann@virtual-identity.com>
2021-10-05 16:29:17 +0200

    fix: 🐛 remove unneeded npm content

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -32,7 +32,7 @@ export class MyComponent {
         <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
           <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
             <span class="block">{this.getText()}</span>
-            <span class="block text-indigo-400">Now enjoyable with Tailwind. 🎉</span>
+            <span class="block text-green-400">Now enjoyable with Tailwind. 🎉</span>
           </h2>
         </div>
       </div>
